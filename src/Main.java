b2e39e84c49ef4d28262aeba18ec01a3e70b7acc
tanishqa321 <tender.tanishka@gmail.com>
2025-01-03 00:44:49 +0530

    UC2:USe of random

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Welcome to Snake & Ladder Game");
         int playerPosition = 0;
+        Random random = new Random();
 
     }
 }
