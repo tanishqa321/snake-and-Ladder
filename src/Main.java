ee98afbbcf89d5bd551503b372e577beb7899c22
tanishqa321 <tender.tanishka@gmail.com>
2025-01-02 21:07:02 +0530

    Added UC1:first player

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Welcome to Snake & Ladder Game");
+        int playerPosition = 0;
 
     }
 }
---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,2 @@
+public class SnakeAndLadder {
+}
